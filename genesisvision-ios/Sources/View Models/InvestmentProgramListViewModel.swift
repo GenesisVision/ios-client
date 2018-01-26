f6099485d2b46ed689d5e5e6c51666bf5ddbf4e5
George Shaginyan <darkglaz13@gmail.com>
2018-01-26 22:30:15 +0300

    Add total count of invest programs from api client

--- genesisvision-ios/Sources/View Models/InvestmentProgramListViewModel.swift
+++ genesisvision-ios/Sources/View Models/InvestmentProgramListViewModel.swift
@@ -23,25 +23,31 @@ class InvestmentProgramListViewModel {
     // MARK: - Variables
     var dataType: DataType = .api
     
-    var skip = 0    //offset
-    var take = 10   //count of programs
+    var skip = 0            //offset
+    var take = 10           //count of programs
+    
+    var totalCount = 0      //total count of programs
     
     var investmentProgramViewModels = [TraderTableViewCellViewModel]()
     
     // MARK: - Public methods
-    func fetch(completion:@escaping () -> Void) {
+    func fetch(completion:@escaping () -> Void?) {
         switch dataType {
         case .api:
             let filter = InvestmentsFilter(managerId: nil, brokerId: nil, brokerTradeServerId: nil, investMaxAmountFrom: nil, investMaxAmountTo: nil, sorting: .byRatingAsc, skip: skip, take: take)
-            apiInvestmentPrograms(withFilter: filter, completion: { [weak self] (investmentPrograms) in
+            apiInvestmentPrograms(withFilter: filter, completion: { [weak self] (investmentProgramsViewModel) in
+                guard let investmentPrograms = investmentProgramsViewModel else { return  }
+                
                 var investmentProgramViewModels = [TraderTableViewCellViewModel]()
                 
-                for investmentProgram in investmentPrograms {
+                self?.totalCount = investmentPrograms.total ?? 0
+                
+                investmentPrograms.investments?.forEach({ (investmentProgram) in
                     let entity = InvestmentProgramEntity()
                     entity.traslation(fromInvestmentProgram: investmentProgram)
                     let traderTableViewCellModel = TraderTableViewCellViewModel(investmentProgramEntity: entity)
                     investmentProgramViewModels.append(traderTableViewCellModel)
-                }
+                })
                 
                 self?.investmentProgramViewModels = investmentProgramViewModels
                 completion()
@@ -54,12 +60,16 @@ class InvestmentProgramListViewModel {
         }
     }
     
-    func fetchMore(completion:@escaping () -> Void) {
+    func fetchMore(completion:@escaping () -> Void?) {
+        if skip >= totalCount {
+            completion()
+        }
+        
         skip += take
         fetch(completion: completion)
     }
     
-    func refresh(completion:@escaping () -> Void) {
+    func refresh(completion:@escaping () ->Void) {
         skip = 0
         fetch(completion: completion)
     }
@@ -87,17 +97,17 @@ class InvestmentProgramListViewModel {
     
     // MARK: - Private methods
     
-    private func apiInvestmentPrograms(withFilter filter: InvestmentsFilter, completion: @escaping (_ traderCellModels: [InvestmentProgram]) -> Void) {
+    private func apiInvestmentPrograms(withFilter filter: InvestmentsFilter, completion: @escaping (_ investmentProgramsViewModel: InvestmentProgramsViewModel?) -> Void) {
         InvestorAPI.apiInvestorInvestmentsPostWithRequestBuilder(filter: filter).execute { (response, error) in
             guard response != nil && response?.statusCode == 200 else {
                 return ErrorHandler.handleApiError(error: error, completion: { (result) in print(result) })
             }
 
-            guard let investmentProgramViewModels = response?.body?.investments else {
-                return completion([])
+            guard let investmentProgramsViewModel = response?.body else {
+                return completion(nil)
             }
             
-            completion(investmentProgramViewModels)
+            completion(investmentProgramsViewModel)
         }
     }
     
