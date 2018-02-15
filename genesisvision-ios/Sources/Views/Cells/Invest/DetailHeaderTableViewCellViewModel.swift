938552e18a6106388fb43f9d99173dff13e8592a
George Shaginyan <darkglaz13@gmail.com>
2018-02-15 16:49:01 +0300

    Fix Profit value

--- genesisvision-ios/Sources/Views/Cells/Invest/DetailHeaderTableViewCellViewModel.swift
+++ genesisvision-ios/Sources/Views/Cells/Invest/DetailHeaderTableViewCellViewModel.swift
@@ -41,10 +41,13 @@ extension DetailHeaderTableViewCellViewModel: CellViewModel {
             let totalProfit = participantViewModel.totalProfit,
             let totalProfitInPercent = participantViewModel.totalProfitInPercent {
             
+            let totalProfitValue = Double(round(100 * totalProfit) / 100)
+            let totalProfitInPercentValue = Double(round(100 * totalProfitInPercent) / 100)
+            
             cell.depositValueLabel.text = String(describing: place)
             cell.tradesValueLabel.text = String(describing: ordersCount)
-            cell.weeksValueLabel.text = String(describing: totalProfit)
-            cell.profitValueLabel.text = String(describing: totalProfitInPercent) + "%"
+            cell.weeksValueLabel.text = String(describing: totalProfitValue)
+            cell.profitValueLabel.text = String(describing: totalProfitInPercentValue) + "%"
             
             cell.depositValueLabel.textColor = UIColor.primary
             cell.tradesValueLabel.textColor = UIColor.Font.medium
