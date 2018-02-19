e6abe128458330b35e9b1fa3f7ec41e12851064e
George Shaginyan <darkglaz13@gmail.com>
2018-02-19 12:52:19 +0300

    Add request review

--- genesisvision-ios/Sources/View Models/WelcomeViewModel.swift
+++ genesisvision-ios/Sources/View Models/WelcomeViewModel.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2018 Genesis Vision. All rights reserved.
 //
 
+import Foundation
+import StoreKit
+
 class WelcomeViewModel {
     
     // MARK: - Variables
@@ -21,6 +24,7 @@ class WelcomeViewModel {
     // MARK: - Public methods
     // MARK: - Navigation
     func start() {
+        requestReview()
         isTournamentApp
             ? startTournament()
             : AuthManager.isLogin()
@@ -41,5 +45,21 @@ class WelcomeViewModel {
     
     private func startTournament() {
         router.show(routeType: .startTournament)
+    }
+    
+    private func requestReview() {
+        let key = Constants.UserDefaults.timesOpened
+        var timesOpened = UserDefaults.standard.integer(forKey: key)
+        
+        timesOpened += 1
+        UserDefaults.standard.set(timesOpened, forKey: key)
+        
+        if timesOpened > 9 {
+            if #available(iOS 10.3, *) {
+                SKStoreReviewController.requestReview()
+            }
+            UserDefaults.standard.removeObject(forKey: key)
+        }
+        
     }
 }
