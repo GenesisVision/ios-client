584f69001f1ab7fa77c1ab5494463ebbf4273e09
MagistrYodaa <lucasdetheroc@gmail.com>
2021-12-01 14:40:42 +0300

    social local changes section founded

---
+++ genesisvision-ios/Sources/Views/Custom Views/ReplierView.swift
@@ -0,0 +1,9 @@
+//
+//  ReplierView.swift
+//  genesisvision-ios
+//
+//  Created by Ruslan Lukin on 01.12.2021.
+//  Copyright © 2021 Genesis Vision. All rights reserved.
+//
+
+import Foundation
---
+++ genesisvision-ios/Sources/Views/Custom Views/ReplyInputField.swift
@@ -0,0 +1,9 @@
+//
+//  ReplyInputField.swift
+//  genesisvision-ios
+//
+//  Created by Ruslan Lukin on 01.12.2021.
+//  Copyright © 2021 Genesis Vision. All rights reserved.
+//
+
+import Foundation
